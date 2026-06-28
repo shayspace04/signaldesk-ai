3e577b35b843d03adb791ad7215caa001cbccfb1
Shayel Panda <shayelpanda@gmail.com>
2026-06-29 04:52:05 +0530

    Remove temporary Lemma SDK clone

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "sonner";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import App from "./App";
-
-import "./index.css";
-
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-
-    <QueryClientProvider client={queryClient}>
-
-      <App />
-
-      <Toaster
-        richColors
-        position="top-right"
-        theme="dark"
-      />
-
-    </QueryClientProvider>
-
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
