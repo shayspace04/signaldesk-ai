f28c30ebce385e226a4251b47cfe9a8bc8e2a8e6
Shayel Panda <shayelpanda@gmail.com>
2026-07-17 00:20:20 +0530

    fix: change Gmail alert recipient to shay24test@gmail.com

--- src/lib/lemmaClient.js
+++ src/lib/lemmaClient.js
@@ -63,6 +63,6 @@ client.auth.subscribe((state) => {
 
 export const ORG_ID = "019ef98f-e90b-74df-9116-d0df1a4baeff";
 export const GMAIL_AUTH_CONFIG = "gmail";
-export const ALERT_RECIPIENT = "shayelpanda@gmail.com";
+export const ALERT_RECIPIENT = "shay24test@gmail.com";
 
 export default client;
